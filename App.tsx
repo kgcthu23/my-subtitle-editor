9ecc9e57560789e7fc396ea810fab8f98af9109e
kaung ce <kgcthu.kev.com>
2026-03-02 12:24:33 +0630

    dsadas

--- App.tsx
+++ App.tsx
@@ -506,7 +506,7 @@ const Guide: React.FC = () => {
                                 <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
                                     <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zM12.75 6a.75.75 0 00-1.5 0v6c0 .414.336.75.75.75h4.5a.75.75 0 000-1.5h-3.75V6z" clipRule="evenodd" />
                                 </svg>
-                                <span>Refreshed: 2026-01-28</span>
+                                <span>Refreshed: 2026-02-28</span>
                             </div>
                         </div>
                         <div className="flex flex-col sm:flex-row gap-3">
